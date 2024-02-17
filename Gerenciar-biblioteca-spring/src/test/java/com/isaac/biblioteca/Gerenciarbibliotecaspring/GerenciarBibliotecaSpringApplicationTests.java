5313e7656fbdcfa7e72d6645fcae240b2386cff1
isaachbt <isaac.silva1478@gmail.com>
2024-02-16 21:31:23 -0300

    Initial commit

---
+++ Gerenciar-biblioteca-spring/src/test/java/com/isaac/biblioteca/Gerenciarbibliotecaspring/GerenciarBibliotecaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaac.biblioteca.Gerenciarbibliotecaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciarBibliotecaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
